03e622890e6b86328a0b2cdabfe9afcb94fb4535
NdamuleloMulaudzi <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2026-09-05 11:50:57 +0200

    Add menu model

---
+++ MenuExplorer/Models/Menu.cs
@@ -0,0 +1,28 @@
+﻿namespace MenuExplorer.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public enum MenuCategory
+        {
+            Appetizer,
+            MainCourse,
+            Dessert,
+            Beverage
+        }
+
+        public MenuCategory Category { get; set; }
+
+        public bool isVegiterian { get; set; }
+
+        public int Rating { get; set; }
+
+    }
+}
