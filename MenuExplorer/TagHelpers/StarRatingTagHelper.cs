d930997413f3a09e7e71e7188a7e5729f5df554e
NdamuleloMulaudzi <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2026-09-06 17:33:41 +0200

    add custom star rating tag helper

---
+++ MenuExplorer/TagHelpers/StarRatingTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MenuExplorer.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("star-rating")]
+    public class StarRatingTagHelper : TagHelper
+    {
+        public int Value { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+
+            var stars = "";
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < Value)
+                {
+                    stars += "<span>&#9733</span>";
+
+                }
+                else
+                {
+                    stars += "<span class=\"empty-star\">&#9733</span>";
+                }
+            }
+            output.Content.SetHtmlContent(stars);
+        }
+    }
+}
